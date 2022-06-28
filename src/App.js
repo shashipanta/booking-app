934ee401a540d5ceb84fbc388120377ac0e19e7f
shashi panta <shashipanta57@gmail.com>
2022-06-28 19:41:27 +0545

    refactor: Add responsive CSS
    Added responsiveness to HeaderSearch component for screen size < 677px.
    
    Fix flotating date picker's position on small screens
    Fix font-size, margin, padding for featured Property list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
+import "./index.css";
 
 function App() {
   return (
