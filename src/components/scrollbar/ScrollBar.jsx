cae90add1a16411454d4baed6432d7bac72c2cba
Shashi Panta <shashipanta57@gmail.com>
2022-07-30 22:39:20 +0545

    Feat: Create Component for animated scrolling content
    This component makes use of basic CSS transitions and mostly animations for
    potraying infinite vertical scrolling list items.
    
    (*) TODO : Update CSS variant with JavaScript one.

---
+++ src/components/scrollbar/ScrollBar.jsx
@@ -0,0 +1,21 @@
+import "./scrollBar.css";
+
+const ScrollBar = () => {
+    return (
+        <>
+        <span className="movingInfo">
+            <ul className="infoWindow">
+                <li className="scrollText">homes</li>
+                <li className="scrollText">apartments</li>
+                <li className="scrollText">vilas</li>
+                <li className="scrollText">condo hotels</li>
+                <li className="scrollText">vacation homes</li>
+                <li className="scrollText">cottages</li>
+                <li className="scrollText">homes</li>
+            </ul>
+        </span>
+        </>
+    )
+}
+
+export default ScrollBar;
