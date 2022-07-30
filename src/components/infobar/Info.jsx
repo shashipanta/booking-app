1a0cf1f8432812b4def1988d20ab94eb5ec1d2e0
Shashi Panta <shashipanta57@gmail.com>
2022-07-30 22:35:08 +0545

    Feat: Create Information containing Component
    InfoBar : is a component that has images along with animated text with fancy
    elements.
    	* leftImg : golden circle that spans outside the container
    	* midBlueContainer: Primary-Blue container with animated text
    				(* animation is still wacky)
    	* rightImg : Illustraion
    It makes use of close button to hide the parent component i.e itself

---
+++ src/components/infobar/Info.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import CloseBtn from "./../ui/closeBtn/CloseBtn";
+import Button from "./../ui/buttons/Button";
+import "./info.css";
+import ScrollBar from "../scrollbar/ScrollBar";
+
+
+const FindInfo = () => {
+    const [showContainer, setShowContainer ] = useState(true);
+
+    function handleShowContainer(showContainer) {
+        setShowContainer(!showContainer);
+    }
+    return (
+        <>
+        {
+            showContainer && 
+            <div className="findInfoContainer">
+                {/* close  */}
+                <CloseBtn liftState={handleShowContainer} />
+
+                <div className="infoLeftImg">
+                </div>
+                <div className="infoBlueContainer">
+                    <span>Find </span>
+                    {/* Automatically Scrolling content  */}
+                    <ScrollBar />
+                    <span>for your next trip.</span>
+                    <Button btnName="Discover more" />
+                </div>
+                <div className="infoRightImg">
+                    <img data-src="https://t-cf.bstatic.com/static/img/bh/awareness/campaign/bh_aw_cpg_main_image/b0f5755d0651acbe9d3f3785a874258150b4393b.png"  src="https://t-cf.bstatic.com/static/img/bh/awareness/campaign/bh_aw_cpg_main_image/b0f5755d0651acbe9d3f3785a874258150b4393b.png"></img>
+                </div>
+
+            </div>
+        }
+        </>
+    )
+}
+
+export default FindInfo;
