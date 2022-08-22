e3f8443f9ce27f1f06e5d2127d4fa08140b9dc28
Shashi Panta <shashipanta57@gmail.com>
2022-08-22 15:25:56 +0545

    Feat: Add Search container in Hotels list page
    Made changes that persist on list page from home page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,12 +1,105 @@
-import React from "react";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import Button from "../../components/ui/buttons/Button";
+import "./list.css";
+import { format, getOverlappingDaysInIntervals } from "date-fns";
+import { DateRange } from "react-date-range";
 
 const List = () => {
+  const location = useLocation();
+
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [openDate, setOpenDate] = useState(false);
+  const [options, setOptions] = useState(location.state.options);
+
+  console.log(location);
   return (
     <div>
       <Navbar />
       <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="lsItem">
+              <label htmlFor="destination">Destination</label>
+              <input
+                name="destination"
+                type="text"
+                // value={location.state.destination}
+                placeholder={destination}
+              />
+            </div>
+            <div className="lsItem">
+              <label htmlFor="">Check-In-Date</label>
+              <span onClick={(e) => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "MM/dd/yyyy"
+              )} to 
+                      ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+
+              {openDate && (
+                <DateRange
+                  onChange={(item) => {
+                    setDate([item.selection]);
+                  }}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+            </div>
+            <div className="lsItem">
+              <label htmlFor="">Options</label>
+              <div className="lsOptions">
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput" />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adult</span>
+                  <input
+                    type="number"
+                    min={1}
+                    placeholder={options.adult}
+                    className="lsOptionInput"
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    placeholder={options.children}
+                    className="lsOptionInput"
+                  />
+                </div>
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Rooms</span>
+                  <input
+                    type="number"
+                    min={1}
+                    placeholder={options.room}
+                    className="lsOptionInput"
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </div>
   );
 };
