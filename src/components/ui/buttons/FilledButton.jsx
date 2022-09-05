8f6aedb12772b86ab51c0cb9a86efad48909b67f
Shashi Panta <shashipanta57@gmail.com>
2022-09-05 17:46:39 +0545

    Refactor:Change console message

--- src/components/ui/buttons/FilledButton.jsx
+++ src/components/ui/buttons/FilledButton.jsx
@@ -1,13 +1,10 @@
 import "./filledButton.css";
 
 function FilledButton(props) {
-  console.log(props.btnType == "primary");
   let btnClass = "filledBtn";
   if (props.btnType == "primary") {
     btnClass = "filledBtn-primary";
   }
-
-  console.log(btnClass);
 
   return <button className={btnClass}>{props.btnName}</button>;
 }
